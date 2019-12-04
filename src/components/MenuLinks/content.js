d3d10525d327af3e77462ea088ca73e3d0f1ea4d
Evandro Macedo <evandro.hcm@gmail.com>
2019-12-03 21:15:31 -0300

    Creates initial part of MenuLinks component

---
+++ src/components/MenuLinks/content.js
@@ -0,0 +1,16 @@
+const links = [
+  {
+    label: 'Blog',
+    url: '/',
+  },
+  {
+    label: 'About',
+    url: '/about',
+  },
+  {
+    label: 'Projects',
+    url: '/projects',
+  },
+]
+
+export default links
---
+++ src/components/MenuLinks/styled.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+
+export const MenuLinksWrapper = styled.nav`
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  padding: 0.8rem 0;
+  position: relative;
+  border-top: 2px solid var(--accent);
+  border-bottom: 2px solid var(--accent);
+`
+
+export const MenuLinksSearch = styled.span`
+  position: absolute;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+`
+
+export const MenuLinksList = styled.ul`
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: 1.6rem;
+`
+
+export const MenuLinksItem = styled.li``
+
+export const MenuLinksLink = styled(Link)`
+  ${props => props.theme.Header_MenuLink}
+  color: var(--text);
+  text-decoration: none;
+  transition: color 0.3s;
+
+  &.active {
+    text-decoration: underline var(--accent);
+  }
+
+  &:hover {
+    color: var(--accent);
+  }
+`
