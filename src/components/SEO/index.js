938de6c40bbe2a985dce15ac93b219521cdf41d3
Evandro Macedo <evandro.hcm@gmail.com>
2019-12-11 02:14:24 -0300

    Format title of twitter card

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -66,7 +66,7 @@ function SEO({ description, lang, meta, title, image }) {
         },
         {
           name: `twitter:title`,
-          content: title,
+          content: `${title} | ${site.siteMetadata.title}`,
         },
         {
           name: `twitter:description`,
