56a05d32c3b41635d8c2f1aae331536ed871f1f9
Evandro Macedo <evandro.hcm@gmail.com>
2019-12-13 04:31:06 -0300

    Move @font-faces load to gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
+import './static/assets/font/font.css'
 import 'lazysizes'
 require('prismjs/themes/prism-tomorrow.css')
 require('prismjs/plugins/line-numbers/prism-line-numbers.css')
--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -1,9 +1,5 @@
 import styled from 'styled-components'
 import theme from '../../styles/theme'
-import GaramondRegular from '../../../static/font/Garamond-Regular.ttf'
-import GaramondItalic from '../../../static/font/Garamond-Italic.ttf'
-import GaramondBold from '../../../static/font/Garamond-Bold.ttf'
-import GaramondBoldItalic from '../../../static/font/Garamond-BoldItalic.ttf'
 import mediaQuery from '../../styles/mediaQuery'
 
 export const LayoutWrapper = styled.section`
@@ -14,35 +10,6 @@ export const LayoutWrapper = styled.section`
   ${mediaQuery.lessThan('header')`
     padding: 0 1.6rem 1.2rem 1.6rem;
   `}
-
-  /* Font-faces */
-  @font-face {
-    font-family: 'Garamond';
-    src: url(${GaramondRegular}) format('truetype');
-    font-weight: normal;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Garamond';
-    src: url(${GaramondItalic}) format('truetype');
-    font-weight: normal;
-    font-style: italic;
-  }
-
-  @font-face {
-    font-family: 'Garamond';
-    src: url(${GaramondBold}) format('truetype');
-    font-weight: bold;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Garamond';
-    src: url(${GaramondBoldItalic}) format('truetype');
-    font-weight: bold;
-    font-style: italic;
-  }
 `
 
 export const LayoutMain = styled.main`
